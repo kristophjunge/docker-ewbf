11d539feae5ea2c04e1eaf9008f9e90822e3865e
Kristoph Junge <kristoph.junge@gmail.com>
2017-10-10 23:44:25 +0200

    First working version

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./miner --server ${SERVER} --user ${ADDRESS}.${WORKER} --pass ${PASS} --port ${PORT} --api
+./miner --server ${SERVER} --user ${ADDRESS}.${WORKER} --pass ${PASS} --port ${PORT} --api 0.0.0.0:42000
