e20d2b89a8d9ac48e6d586afe806c300bd8c2b72
Kristoph Junge <kristoph.junge@gmail.com>
2017-10-11 00:05:50 +0200

    Added startup delay

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -e
+
+echo "Waiting ${STARTUP_DELAY} seconds"
+
+sleep ${STARTUP_DELAY}
+
+echo "Starting miner"
 
 ./miner --server ${SERVER} --user ${ADDRESS}.${WORKER} --pass ${PASS} --port ${PORT} --api 0.0.0.0:42000
